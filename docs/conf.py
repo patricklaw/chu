2f710f25a7dc8912e982a87ee357158f5c9afb28
Patrick Lawson <plawson@shopwiki.com>
2013-01-30 12:12:03 -0500

    Special case tornado.gen.engine in Mock object (return an identity lambda)

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ from os.path import dirname, abspath
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 ## From http://read-the-docs.readthedocs.org/en/latest/faq.html#my-project-isn-t-building-with-autodoc
+
 class Mock(object):
     def __init__(self, *args, **kwargs):
         pass
@@ -49,6 +50,8 @@ class Mock(object):
     def __getattr__(cls, name):
         if name in ('__file__', '__path__'):
             return '/dev/null'
+        elif name == 'engine':
+            return lambda x: x
         elif name[0] == name[0].upper():
             mockType = type(name, (), {})
             mockType.__module__ = __name__
