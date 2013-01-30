9a0b548627c371514d520f86c5ca36b9e7e0a3a3
Patrick Lawson <plawson@shopwiki.com>
2013-01-30 11:49:34 -0500

    Mock tornado, pika, and simplejson when building Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,31 @@ from os.path import dirname, abspath
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
+## From http://read-the-docs.readthedocs.org/en/latest/faq.html#my-project-isn-t-building-with-autodoc
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['tornado', 'pika', 'simplejson']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
